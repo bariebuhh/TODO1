ea5b6e3444545e833cc4307026980b664badef15
bariebuhh <90452383+bariebuhh@users.noreply.github.com>
2022-09-12 17:21:11 +0400

    v1

---
+++ js/todo.js
@@ -0,0 +1,16 @@
+const field = document.querySelector('.field');
+const button = document.querySelector('.add');
+const divList = document.querySelector('.list');
+
+function createTask (value){
+    const task = document.createElement("div");
+    task.textContent = value 
+    return task;
+}
+function addTask (){
+    if (field.value){
+         const newTask = createTask(field.value)
+        divList.appendChild(newTask)
+        field.value = '';
+}
+button.addEventListener('click', addTask)
